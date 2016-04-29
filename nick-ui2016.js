283760cdf8e111a1233946f77eb0a0144f49a534
nikeshpathania <nikesh.pathania@gmail.com>
2016-04-29 23:59:23 +0530

    add var for_testing

--- nick-ui2016.js
+++ nick-ui2016.js
@@ -1,10 +1,10 @@
 // JavaScript Document
 // Get lenth of .colors_pricebox Table
 // addClass into qty table
-// .colors_pricebox Table InsertAfter Option_Table 
+// .colors_pricebox Table InsertAfter Option_Table
 // Change dynamic class name of colors_pricebox Table
 // Insert Description to after price Offer
-// Inser Product code to after Product Heading 
+// Inser Product code to after Product Heading
 var DefattachmentImgSrc;
 var DefribbonImgSrc;
 var DefmedalImgSrc;
@@ -15,12 +15,12 @@ var product_code = getProductCode();
 var protocol = (window.location.protocol); // Get Http
 var host_name = (window.location.hostname); // Get Host name i.e. workondesk.in
 
+var for_testing;
 
 
-	
 function loadDesignerTemp(){
-klonTable();		
-changeQtyTable();	
+klonTable();
+changeQtyTable();
 addCointainerForText();
 insertAfterItems();
 engravingTrInsert();
@@ -42,7 +42,7 @@ function closed_popup(){
 			$("#show_engravings").hide();
 			$("#engravingStatus").val('0');
             $("#cloned_btn").css('display', 'none');
-            $(".vCSS_input_addtocart").show();			
+            $(".vCSS_input_addtocart").show();
 		}
 		else
 		{
@@ -50,7 +50,7 @@ function closed_popup(){
 			$("#show_engravings").hide();
             $(".vCSS_input_addtocart").show();
 		}
-				
+
 	if($('#popup2').css('display')=='block'){
 		$('.engraving_option input').each(function () {
         var idStringFull = ($(this).attr('id'));
@@ -62,14 +62,14 @@ function closed_popup(){
         //$("#line" + idValueInteger).replaceWith(completeInput);
         $("#" + $(this).attr('id') + "_display").html($(this).val());
 		$(this).val('');
-		});	
+		});
 	}
 
 }
 function clearPopUp(){
         $('#line1').val('');
         $('#line2').val('');
-        $('#line3').val('');		
+        $('#line3').val('');
 	if($('#popup2').css('display')=='block'){
 		$('.engraving_option input').each(function () {
         var idStringFull = ($(this).attr('id'));
@@ -81,7 +81,7 @@ function clearPopUp(){
        // $("#line" + idValueInteger).replaceWith(completeInput);
         $("#" + $(this).attr('id') + "_display").html($(this).val());
 		$(this).val('');
-		});	
+		});
 	}
 
 }
@@ -96,7 +96,7 @@ function showalert()
 $( document ).ready(function() {
 
 $tmc = $(".vCSS_input_addtocart").clone().attr('id', 'cloned_btn').show();
-$tmc.insertAfter(".vCSS_input_addtocart"); 
+$tmc.insertAfter(".vCSS_input_addtocart");
 $("#cloned_btn").removeAttr('onclick','');
 $("#cloned_btn").html('');
 $("#cloned_btn").replaceWith('<img src="/v/vspfiles/templates/253/images/buttons/btn_addtocart.gif" id="cloned_btn" class="btnN" name="btnaddtocart" alt="Add to cart" border="0" onclick="showalert();">');
@@ -104,11 +104,11 @@ $("#cloned_btn").css('float','right');
 $("#cloned_btn").css('display','none');
 
 $('#popup1_no').click(function() {
-	if ($(this).is(':checked')){		
+	if ($(this).is(':checked')){
             $("#engravingStatus").val('0');
             $("#cloned_btn").css('display', 'none');
             $(".vCSS_input_addtocart").show();
-       	} 
+       	}
 
 });
 
@@ -126,12 +126,12 @@ $('select#optional_engraving').change(function () {
             $("#cloned_btn, #show_engravings").css('display', 'none');
             $(".vCSS_input_addtocart").show();
         }
-	
+
 });
 /* End Validation Purpose*/
 
 $(".vCSS_input_addtocart, #cloned_btn").click(function(){
-	
+
 		var reset_status = $("#engravingStatus").val();
 		//alert(reset_status); return false;
 	   if(reset_status == 0)
@@ -142,9 +142,9 @@ $(".vCSS_input_addtocart, #cloned_btn").click(function(){
 			$('#options_table input').val('');
 			storeDefaultImagePaths();
 		}
-	}); 
+	});
 	$('#optional_engraving').change(function(){
-		
+
 		//var val = $(this).find("option::selected").text();
 		var val = $('option:selected', $(this)).text();
 		//alert(val.toLowerCase());
@@ -154,12 +154,12 @@ $(".vCSS_input_addtocart, #cloned_btn").click(function(){
 		else{
 			//alert('no');
 		}
-			
+
 	});
-	
-	
-	
-	
+
+
+
+
 // For Engraving Button
 if (show_engraving_button_nick()) {
             $(".v65-productdetail-cartqty").before('<img onclick="engravingPopUp()" id="show_engravings" src="http://' + document.domain + '/v/vspfiles/images/Add-engraving-button.png" alt="Add Engraving" style="float:right" />');
@@ -195,8 +195,8 @@ if (show_engraving_button_nick()) {
                 $('.product_productprice').find('b').find('span[itemprop="price"]').text("$" + new_price.toFixed(2));
 
             });
-        }	
-	
+        }
+
 });
 
 //var localdate = ShowLocalDate();
@@ -210,20 +210,20 @@ if( $('#mediafarmplugin').length == 1 )
 		 $('#mediafarmplugin').insertAfter('#product_photo_zoom_url, .vCSS_img_product_photo');
 		$('#product_photo_zoom_url2').remove();
 	}
-	
+
 var tablecount = $('.colors_pricebox').length;
 $('.v65-productdetail-cartqty').parent().parent().parent().parent().addClass('qty_table');
-$('.colors_pricebox').each(function(index, element) {		
+$('.colors_pricebox').each(function(index, element) {
 		$(this).attr('id','table'+index );
 	});
 	if(tablecount>=2){
-		$('#table0').insertAfter('#table1'); 
+		$('#table0').insertAfter('#table1');
 	}
 }
 
 
 function changeQtyTable(){
-var table = $('.qty_table'), 
+var table = $('.qty_table'),
     div = $('<div />');
 	$('td', table).each(function(i){
 		$('<div class="col' + (i+1) + '" />').html($(this).html()).appendTo(div);
@@ -231,13 +231,13 @@ var table = $('.qty_table'),
 	table.after(div).remove();
 		$('.col1').parent().addClass('qty_table');
 }
-	
-	
-function addCointainerForText(){	
+
+
+function addCointainerForText(){
 	$('td:contains("Click Add Engraving button after selecting this option to enter text details")').html(function (_, html){
 		return  html.replace(/(Click Add Engraving button after selecting this option to enter text details)/g, '<small class="small_text">$1</small>')
 		});
-		
+
 	$('.col1:contains("Qty:")').html(function (_, html){
 		return  html.replace(/(Qty:)/g, '<span class="Qty_label">$1</span>')
 		});
@@ -247,7 +247,7 @@ function insertAfterItems(){
 	$("div[itemprop=offers]").insertAfter('.qty_table');
 	$("span[itemprop=description]").insertAfter("span#product_description");
 	$("div[itemprop=offers] i").insertAfter('.productnamecolorLARGE');
-	
+
 	$('.colors_pricebox').parent().addClass('rightTd');
 	$('.rightTd').prev().addClass('leftTd');
 	$('#options_table').find('img').parent('td').addClass('options_text');
@@ -268,8 +268,8 @@ function engravingTrInsert(){
 	   $(this).closest("tr").css("display","none").addClass('no');
 	   $(this).closest("tr input[type='text']").attr('id','line'+(index+1));
      });
-	 
-	 
+
+
 	$('.engraving_option input').each(function(index, element) {
          $(this).attr('id','tags'+index );
 		  $(this).attr('maxlength',25 );
@@ -277,23 +277,23 @@ function engravingTrInsert(){
 		   var indexCounter = (index + 1);
 		   jQuery("#line"+indexCounter).val($(this).val());
 		   $("#" + $(this).attr('id') + "_display").html($(this).val());
-		   
-		   
+
+
     });
-	
- 	$('.engraving_option input').change(function() {		
-	
+
+ 	$('.engraving_option input').change(function() {
+
 		var x = $("#tags0").val();
         var y = $("#tags1").val();
         var z = $("#tags2").val();
         $('#line1').val(x);
         $('#line2').val(y);
         $('#line3').val(z);
-			
+
 			$("#cloned_btn").css('display','none');
 			$(".vCSS_input_addtocart").show();
 			$("#engravingStatus").val('0');
-	}); 
+	});
 
 	/* For Medal Text*/
 	/*var lineArr = ['CONFIGURE','ENGRAVING OPTION','TO PREVIEW HERE'];
@@ -301,21 +301,21 @@ function engravingTrInsert(){
 	for(var j=0;j<3;j++)
 	{
 		medalhtml += '<p style="font-size:9px"><strong><span id="tags'+j+'_display">'+lineArr[j]+'</span></strong></p>';
-	}	
+	}
 	$('#medal_text').html(medalhtml);*/
-	
-	
+
+
 	//var reff = $('#options_table').find('tr').slice(10,11).find('input[type="text"]').val();
 	//alert(reff);
 	//$('.popup3_row1').next().html('<p style="color:#ff0000">' +localdate+ ' Refference '+ reff +' </p>');
-		
+
 
 	//$('.engraving_option').after('<div style="text-align:right"><input type="image" src="/v/vspfiles/images/update_button_popup.png"  onclick="save_engraving()" value="Save Engravings" id="save_engraving" class="b-close single_add_to_cart_button button alt"></div>');
-	 
+
 	// $('#options_table tr:last').after('<tr style="display:none"><td colspan="3" id="tdHtml"><input type="text" name="line1" id="line1" value="">	<input type="text" name="line2" id="line2" value="">	<input type="text" name="line3" id="line3" value=""></td></tr>');
-	 
+
 	//alert(1);
-	
+
 
 }
 
@@ -323,24 +323,24 @@ function engravingTrInsert(){
 function SelectChangeHandler()
 {
 	var selctId = $(this).attr('id');
-	
+
 	if (selctId != undefined ){
 	var yourString = selctId.replace ( /[^\d.]/g, '' );
 	var divid = "#style"+yourString;
-	
+
 	if( $(divid).length == 1)
-	{	
+	{
 	var commonImg = "style"+yourString;
-	var productCode = getProductCode().toLowerCase();	
+	var productCode = getProductCode().toLowerCase();
 	var imgString = ($(divid+" > img").attr('src'));
 	//alert(imgString);
 	//alert(pathVar);
-	
+
 		if( imgString.indexOf('commonfiles') >= 1 )
 		{
-			
+
 			var folderArry = imgString.split('commonfiles');
-			var pathSTring = (folderArry[1]);	
+			var pathSTring = (folderArry[1]);
 			pathSTring = pathSTring.slice(1);
 			//alert(pathSTring);
 			var subPath = pathSTring.split('/');
@@ -349,15 +349,15 @@ function SelectChangeHandler()
 			for( var j=0; j < (subPath.length-1); j++ )
 			{
 				pathVar += '/'+subPath[j];
-				
+
 			}
-			
+
 			var folderURL ='http://expressmedalscom.ipage.com/product_designers/commonfiles'+pathVar+"/";
 		} else if (imgString.indexOf(productCode) >= 1 )
 		{
 			var folderArry = imgString.split(productCode);
-			
-			var pathSTring = (folderArry[1]);	
+
+			var pathSTring = (folderArry[1]);
 			pathSTring = pathSTring.slice(1);
 			//alert(pathSTring);
 			var subPath = pathSTring.split('/');
@@ -366,27 +366,27 @@ function SelectChangeHandler()
 			for( var j=0; j < (subPath.length-1); j++ )
 			{
 				pathVar += '/'+subPath[j];
-				
+
 			}
-			
+
 			//alert(folderArry);
 			var folderURL ='http://expressmedalscom.ipage.com/product_designers/'+productCode+''+pathVar+'/';
 		}
-		
-		
+
+
 		else
 		{
 			var folderURL ='http://expressmedalscom.ipage.com/product_designers/'+productCode+'/';
 		}
 		//alert(folderURL);
-	
+
 	if (selctId == selctId) {
 		  var name = $('option:selected',this).text();
 		  //alert(name);
-          var img = getImageName(name,this.value);            
-          var imgSrc = folderURL+img+'.png';  
+          var img = getImageName(name,this.value);
+          var imgSrc = folderURL+img+'.png';
 		// getStaticDiv();
-		
+
 		if($(divid).length == 1)
 		{
 			$(divid+" > img")[0].src = imgSrc;
@@ -394,7 +394,7 @@ function SelectChangeHandler()
 	}
 	}
 	}
-	
+
 }
 
 function getImageName(name,value)
@@ -409,8 +409,8 @@ function getImageName(name,value)
            var name5 = name4.replace(/\//g,"_");
            var name6 = name5.replace(/-/g,"_");
            var name7 = name6.replace(/#/g,"");
-         
-           //alert(a.replace( /\s/g, "")); 
+
+           //alert(a.replace( /\s/g, ""));
             return 'w_'+value+'_a_'+name7.toLowerCase();
 
 		}else
@@ -421,9 +421,9 @@ function getImageName(name,value)
            var name3 = name2.replace( /\s/g, "_");
            var name4 = name3.replace( /"/g, "");
            var name5 = name4.replace(/\//g,"_");
-           var name6 = name5.replace(/-/g,"_");  
-           var name7 = name6.replace(/#/g,"");                
-         
+           var name6 = name5.replace(/-/g,"_");
+           var name7 = name6.replace(/#/g,"");
+
            return 'w_'+value+'_a_'+name7.toLowerCase();
 		}
 
@@ -433,49 +433,49 @@ function getImageName(name,value)
 
 function addSelectOptionId(){
 	// Embed Static Div Function
-	var embedDIV =jQuery('#mediafarmplugin > div');                                                        
+	var embedDIV =jQuery('#mediafarmplugin > div');
 	var countDiv= embedDIV.size();
-	
+
 	//for( var divcnt = 1;divcnt <= countDiv;divcnt++ )
-	//{	
+	//{
 	jQuery('#options_table >tbody >tr >td.options_text').each(function(idx,obj){
 		var trRowCount = Number(idx)+1;
 		//alert(trRowCount);
 		if (trRowCount <= countDiv){
-			
-			
+
+
 		var trid = "tableStyle"+(Number(idx)+1);
 			$(this).parent().attr('id',trid);
-		
-		
-		
+
+
+
 		var IDName = 'selected'+(Number(idx)+1);
 		//alert(IDName);
-				
+
 				jQuery(this).next().next().find('select').attr('id',IDName);
 				jQuery(this).next().next().find('select').change(SelectChangeHandler);
-		}		
-		
+		}
+
 			});
-			
+
 		$('#options_table >tbody >tr >td').next().next().find('select').change(SelectChangeHandler);
 		$('td:contains("Optional Engraving")').next().next().find('select').attr('id','optional_engraving');
-		
-	
+
+
 	//}
 }
 function getDefaultImages()
 {
-	var embedDIV =jQuery('#mediafarmplugin > div');                                                        
+	var embedDIV =jQuery('#mediafarmplugin > div');
 	var countDiv= embedDIV.size();
-	
+
 	for( var j=1;j<=countDiv;j++)
 	{
-		
+
 		var divId = "#style"+j;
-		
+
 		var imgString = ($(divId+" > img").attr('src'));
-		
+
 		defaultImgArr[j] = (imgString);
 	}
 	return defaultImgArr;
@@ -483,22 +483,22 @@ function getDefaultImages()
 
 
 function storeDefaultImagePaths()
-{	
+{
 	var imgLength = (defaultImgArr.length);
-	var embedDIV =jQuery('#mediafarmplugin > div');                                                        
+	var embedDIV =jQuery('#mediafarmplugin > div');
 	var countDiv= embedDIV.size();
-	
+
 	for( var j=1;j<=countDiv;j++)
 	{
-		
+
 		var divId = "#style"+j;
-		
+
 		var imgString = ($(divId+" > img").attr('src',defaultImgArr[j]));
-		
+
 		//defaultImgArr.push(imgString);
 	}
-	
-	
+
+
 }
 
 function resetForm1(){
@@ -508,10 +508,10 @@ function resetForm1(){
 	$("#show_engravings").hide(); // hide engraving button //
 	var productCode = getProductCode();
 
-	$('.product_productprice').find('b').find('span').text(defaultProductPrice);	
-	
+	$('.product_productprice').find('b').find('span').text(defaultProductPrice);
+
 	/* For Medal Text*/
-	
+
 /*	var lineArr = ['CONFIGURE','ENGRAVING OPTION','TO PREVIEW HERE'];
 	var medalhtml = '';
 	for(var j=0;j<3;j++)
@@ -532,15 +532,15 @@ $.fn.clearForm = function() {
 	{
 		var boxname = this.name;
 		if( boxname.indexOf('QTY') >=0 ) // for qty input box //
-		{		
+		{
 			this.value = '1';
 		}
-		
+
 		if(this.id == 'line1')
 		{
 			$(this).val('');//CONFIGURE
-			$(this).attr('placeholder','Your engraving text here');		
-			
+			$(this).attr('placeholder','Your engraving text here');
+
 		}
 		if(this.id == 'line2')
 		{
@@ -552,7 +552,7 @@ $.fn.clearForm = function() {
 			$(this).val(''); //TO PREVIEW HERE
 			$(this).attr('placeholder','Your engraving text here');
 		}
-	}  
+	}
 	else if (type == 'radio')
       this.checked = false;
    else if (tag == 'select')
@@ -565,7 +565,7 @@ $.fn.clearForm = function() {
 
 /* Validation */
     var specialKeys = new Array();
-        specialKeys.push(8); 
+        specialKeys.push(8);
         $(function () {
             $("#popup1_qty,.v65-productdetail-cartqty").bind("keypress", function (e) {
                 var keyCode = e.which ? e.which : e.keyCode
@@ -583,7 +583,7 @@ $.fn.clearForm = function() {
 
 /* /Validation */
 /* .Resest Form2 */
-function resetForm2(){	
+function resetForm2(){
 //alert('Rest form2 Successfully');
 $.fn.clearForm = function() {
   return this.each(function(index) {
@@ -592,15 +592,15 @@ $.fn.clearForm = function() {
    if (tag == 'form')
 	{
       return $(':input',this).clearForm();
-	}     
+	}
    if (tag == 'select')
    {
      if( $.trim(boxid) == 'optional_engraving')
 	  {
 		  this.selectedIndex = 0;
 	  }
-	  
-	}  
+
+	}
   });
 };
 	$('#vCSS_mainform').clearForm();
