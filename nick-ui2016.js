d1a8e18787ea5dcc07ce449539463558e7f3e2d2
nikeshpathania <nikesh.pathania@gmail.com>
2016-04-29 23:37:06 +0530

    first testing
    Add Testing var

--- nick-ui2016.js
+++ nick-ui2016.js
@@ -15,6 +15,8 @@ var product_code = getProductCode();
 var protocol = (window.location.protocol); // Get Http
 var host_name = (window.location.hostname); // Get Host name i.e. workondesk.in
 
+var TestingGit
+
 
 	
 function loadDesignerTemp(){
